b0bbe5eb18c5335f93c40f15b1b85b39b07d47b3
anirudhvodela123 <88586486+anirudhvodela123@users.noreply.github.com>
2022-09-14 14:54:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+var port = 3000;
 
 app.use(express.json())
 
@@ -16,4 +16,4 @@ mongoose.connect('mongodb+srv://anirudh:anirudh@cluster0.ulq3gxg.mongodb.net/tes
 require("./routes/routes")(app);
 
 
-app.listen(3000, () => console.log('server running in 3000'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
